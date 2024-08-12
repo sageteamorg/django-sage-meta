d7e97ec6c2aa5e8210c1ee210b32368f25cfa753
radinceorc <radin.ceorc77@gmail.com>
2024-08-12 20:47:43 +0330

    refactor(django_sage_meta): Remove the additional signal

--- django_sage_meta/apps.py
+++ django_sage_meta/apps.py
@@ -4,5 +4,3 @@ from django.apps import AppConfig
 class DjangoSageFacebookConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "django_sage_meta"
-    def ready(self):
-        import django_sage_meta.signals
