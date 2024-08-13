e8678001da94b21e3b2f1b790027b88ed77daab4
radinceorc <radin.ceorc77@gmail.com>
2024-08-13 21:47:23 +0330

    refactor(helper): Add new enum

--- django_sage_meta/helper/choice.py
+++ django_sage_meta/helper/choice.py
@@ -4,3 +4,8 @@ from django.db import models
 class ContentFileEnum(models.TextChoices):
     image = ("image", "IMAGE")
     videos = ("videos", "VIDEOS")
+
+
+class InsightKindEnum(models.TextChoices):
+    account = ("account", "ACCOUNT")
+    media = ("media", "MEDIA")
